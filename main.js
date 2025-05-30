5e3122a3da3987a1a417b5f62ecc0b5be387e16c
Lone <112120875+windnedr@users.noreply.github.com>
2025-05-30 14:42:05 -0500

    Initial move off PC

---
+++ main.js
@@ -0,0 +1,74 @@
+var mousex = 0
+var mousey = 0
+
+function mouse(event) {
+    mousex = event.clientX;
+    mousey = event.clientY;
+
+    document.body.style = "background-position: " + mousex / 2 + "px " + mousey / 2 + "px;"
+    document.getElementById("cursglow").style = "background-position: " + ((mousex / -2) - 0) + "px " + ((mousey / -2) - 0) + "px; top: " + (mousey - 150) + "; left: " + (mousex - 150)
+    document.getElementById("curs").style = "top: " + (mousey - 283 / 2) + "px; left:" + (mousex - 283 / 2) + ";"
+    // document.getElementById("center").style = "top: calc(50vh / 2 - 10px - " + (-mousey / 50) + "px); left: calc(50vh / 2 - 10px - " + (-mousex / 50) + "px)"
+}
+
+function home() {
+    if (document.getElementById("center")){
+        document.getElementById("center").remove()
+    }
+
+    var dv = document.createElement("div");
+    dv.id = "center"
+    dv.innerHTML = "Windnedr<br><br><a onclick='proj()'>Projects</a><br><a onclick='abt()'>About Site</a><br>"
+    document.body.appendChild(dv)
+}
+
+function abt() {
+    if (document.getElementById("center")){
+        document.getElementById("center").remove()
+    }
+
+    var dv = document.createElement("div");
+    dv.id = "center"
+    dv.innerHTML = "<img src='./assets/cowl.png' style='width: 100%;height: 40%;image-rendering: pixelated; object-fit: cover;padding-bottom: 8px;'> <br> About<br><br>Made in pure HTML, CSS, and Javascript"
+    document.body.appendChild(dv)
+}
+
+function proj() {
+    if (document.getElementById("center")){
+        document.getElementById("center").remove()
+    }
+
+    var dv = document.createElement("div");
+    dv.id = "center"
+    dv.innerHTML = "Projects<br><br>"
+    dv.innerHTML += "<a onclick='pom()'>POMR</a><br>"
+    document.body.appendChild(dv)
+}
+
+function pom() {
+    if (document.getElementById("center")){
+        document.getElementById("center").remove()
+    }
+
+    var dv = document.createElement("div");
+    dv.id = "center"
+    dv.innerHTML = "<img src='./assets/thumbnails/POMR wind banner.png' style='width: 100%;height: 40%;aspect-ratio: 4 / 1 !important;image-rendering: pixelated; object-fit: cover;padding-bottom: 8px;'>"
+    dv.innerHTML += "<br> POMR<br> <a href='https://pomr.org'>OPEN</a> <br><br>Made with Love and care! (also HTML, CSS, JS, and PHP) <br><hr>"
+    dv.innerHTML += "Desc.<br>POMR is a FOSS streaming service with User Generated Content. <br><br>Currently, the only media types are Shows & Movies.<br>More media types might be added if there is user demand."
+    document.body.appendChild(dv)
+}
+
+function pom() {
+    if (document.getElementById("center")){
+        document.getElementById("center").remove()
+    }
+
+    var dv = document.createElement("div");
+    dv.id = "center"
+    dv.innerHTML = "<img src='./assets/thumbnails/POMR wind banner.png' style='width: 100%;height: 40%;image-rendering: pixelated; object-fit: cover;padding-bottom: 8px;'>"
+    dv.innerHTML += "<br> POMR<br> <a href='https://pomr.org'>OPEN</a> <br><br>Made with Love and care! (also HTML, CSS, JS, and PHP) <br><hr>"
+    dv.innerHTML += "Desc.<br>POMR is a FOSS streaming service with User Generated Content. <br><br>Currently, the only media types are Shows & Movies.<br>More media types might be added if there is user demand."
+    document.body.appendChild(dv)
+}
+
+home()
